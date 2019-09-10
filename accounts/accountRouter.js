bae34041da6aaa9f39a3aa8b8335807dac0276af
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-09-10 18:06:56 -0500

    setup endpoints

--- accounts/accountRouter.js
+++ accounts/accountRouter.js
@@ -41,7 +41,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
     const { id } = req.params; 
-    db('accounts').where({ id }).del()
+    db('accounts').where({ id }).del() 
     .then(accounts => {
         res.status(200).json(accounts)
     })
